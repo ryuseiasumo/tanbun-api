3e8998403b3864257dfabd3bc0d60122593f7e70
ryuseiasumo <ryusao5656@icloud.com>
2020-05-02 13:57:09 +0900

    echoの大枠作成

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ryuseiasumo/tanbun-api
 
 go 1.14
+
+require github.com/labstack/echo/v4 v4.1.16 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+  "net/http"
+  "github.com/labstack/echo/v4"
+  "github.com/labstack/echo/v4/middleware"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
 
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+// map作成
+var id_message map[string]string
+
+func init() {
+	id_message = map[string]string{
+		"123456":
+	}
 }
 
 func main() {
-	handleRequests()
+  // Echo instance
+  e := echo.New()
+
+  // Middleware
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // Routes
+  e.GET("/", getid)
+  //e.POST()
+
+  // Start server
+  e.Logger.Fatal(e.Start(":8081"))
 }
+
+// Handler
+func getid(c echo.Context) error {
+	// 6桁のidを取得した時
+	id := c.QueryParam("id")
+
+	return c.JSON(http.StatusOK, map[string]string{
+        "id": id,
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{"hello": greetingto})
+  }
