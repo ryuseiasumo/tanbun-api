a18f554a41e80eab399f188ce75b289abfc901ae
ryuseiasumo <ryusao5656@icloud.com>
2020-05-02 16:29:06 +0900

    Post生成

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/ryuseiasumo/tanbun-api/types"
 	"net/http"
+	"fmt"
+	"time"
+	"math/rand"
 )
 
 // map作成
@@ -20,16 +23,34 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	rand.Seed(time.Now().UnixNano())
+
 	idMessage.Init()
 	idMessage.Set("123456", "ジョンソン")
 
 	// Routes
 	e.GET("/", getid)
-	//e.POST()
+	e.POST("/", postmessage)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8081"))
 }
+
+
+func generateUniqueId() string {
+	var idtmp int
+	var idstr string
+
+	for {
+		idtmp = rand.Intn(1000000)
+		idstr = fmt.Sprintf("%06d",idtmp)
+		if !(idMessage.ExistKey(idstr)) {
+			return idstr
+		}
+	}
+}
+
+
 
 // Handler
 func getid(c echo.Context) error {
@@ -45,4 +66,26 @@ func getid(c echo.Context) error {
 
 		return c.JSON(http.StatusNotFound, apierr)
 	}
+}
+
+
+// MessageParam は /api/hello が受けとるJSONパラメータを定義します。
+type MessageParam struct {
+    Message string `json:"message"`
+}
+
+func postmessage(c echo.Context) error {
+	// メッセージを取得した時
+	message := new(MessageParam)
+
+	if err := c.Bind(message); err != nil {
+        return err
+	}
+
+	id := generateUniqueId()
+
+	idMessage.Set(id, message.Message)
+
+	return c.JSON(http.StatusOK, map[string]string{"id": id})
+
 }
